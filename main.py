d873460bb030358e6ccb82afb6116b567ec1bf01
holyyeti <sebastian.barzaghi@outlook.com>
2024-09-01 00:41:11 +0200

    FEAT: showing player

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
+    p = Player(
+        SCREEN_WIDTH / 2, 
+        SCREEN_HEIGHT / 2,
+        PLAYER_RADIUS)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('#000000')
+        p.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == '__main__':
     main()
