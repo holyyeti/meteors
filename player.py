90857001da02f732efd00c0fdb09da01c9348eb1
holyyeti <sebastian.barzaghi@outlook.com>
2024-09-01 11:17:58 +0200

    FEAT: working player movement

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
 from circleshape import CircleShape
 import pygame
+from constants import *
 
 class Player(CircleShape):
 
@@ -22,8 +23,15 @@ class Player(CircleShape):
     def update(self, dt):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_a]:
-            self.rotation = 90
+            self.rotation -= 2
         if keys[pygame.K_d]:
-            self.rotation = -90
-        
+            self.rotation += 2
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(dt * -1)
         pygame.Vector2(0, 1).rotate(self.rotation)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
