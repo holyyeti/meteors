f256f7e9c19b916a23e627a5ed15ded06dda5c7e
holyyeti <sebastian.barzaghi@outlook.com>
2024-09-01 16:37:59 +0200

    FEAT: working spawning asteroids

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
     pygame.init()
@@ -11,7 +14,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+
+    field = AsteroidField()
 
     p = Player(
         SCREEN_WIDTH / 2, 
