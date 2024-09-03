e7699c8b4f5243603e3d601fd350ac07ceac2f98
holyyeti <sebastian.barzaghi@outlook.com>
2024-09-03 22:09:20 +0200

    FEAT: working rate limit

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
         self.rotation = 0
+        self.shoot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -34,14 +35,17 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             self.shoot(dt)
         pygame.Vector2(0, 1).rotate(self.rotation)
+        self.shoot_timer -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self, dt):
-        shot = Shot(self.position[0], self.position[1], SHOT_RADIUS)
-        shot.velocity = pygame.Vector2(0, 1)
-        shot.velocity = shot.velocity.rotate(self.rotation)
-        shot.velocity *= PLAYER_SHOOT_SPEED
+        if self.shoot_timer <= 0:
+            shot = Shot(self.position[0], self.position[1], SHOT_RADIUS)
+            shot.velocity = pygame.Vector2(0, 1)
+            shot.velocity = shot.velocity.rotate(self.rotation)
+            shot.velocity *= PLAYER_SHOOT_SPEED
+            self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         
